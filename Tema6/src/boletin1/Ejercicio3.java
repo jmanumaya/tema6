398d3de748d2f48dc305ae2671d7e52ecc527e72
[name] <[jm.maya@iesnervion.es]>
2025-04-22 14:37:57 +0200

    empezado

--- Tema6/src/boletin1/Ejercicio3.java
+++ Tema6/src/boletin1/Ejercicio3.java
@@ -1,9 +1,24 @@
 package boletin1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		
+		
+		try (BufferedReader bf = new BufferedReader(new FileReader("src\\boletin1.archivos\\ejercicio3"))){
+			
+			
+			
+		} catch (FileNotFoundException e) {			
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 
 	}
