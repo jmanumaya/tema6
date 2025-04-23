2a42552c466321746a6ec0acb29920dcd4669a9d
[name] <[jm.maya@iesnervion.es]>
2025-04-23 09:15:30 +0200

    Terminado

--- Tema6/src/boletin1/Ejercicio3.java
+++ Tema6/src/boletin1/Ejercicio3.java
@@ -4,21 +4,60 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Ejercicio3 {
 
+	private static final String RUTA_TXT = "src\\boletin1\\archivos\\ejercicio3.txt";
+
 	public static void main(String[] args) {
 		
+		String line;
 		
-		try (BufferedReader bf = new BufferedReader(new FileReader("src\\boletin1.archivos\\ejercicio3"))){
+		String datos[];
+		
+		int mediaEdades = 0;
+		
+		int sumaEdades = 0;
+		
+		int count = 0;
+		
+		double mediaAlturas = 0;
+		
+		double sumaAlturas = 0;
+		
+		try (BufferedReader bf = new BufferedReader(new FileReader(RUTA_TXT))){
 			
+			line = bf.readLine();
 			
+			while(line != null) {
+				
+				line = line.trim();
+				
+				datos = line.split(" ");
+				
+				System.out.println(line);
+
+				sumaEdades += Integer.parseInt(datos[1]);
+				sumaAlturas += Double.parseDouble(datos[2]);
+				
+				++count;
+				
+				line = bf.readLine();
+			}
+			
+			mediaEdades = sumaEdades / count;
+			mediaAlturas = sumaAlturas / count;
+			
+			System.out.println("Media de Edades: " + mediaEdades);
+			System.out.println("Media de Alturas: " + mediaAlturas);
 			
 		} catch (FileNotFoundException e) {			
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 		
 
 	}
