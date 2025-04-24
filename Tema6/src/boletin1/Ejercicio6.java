7ab44df350747161ce6b239487ff75b783122ba0
jmanumaya <jm.maya@iesnervion.es>
2025-04-24 22:18:12 +0200

    Terminado

---
+++ Tema6/src/boletin1/Ejercicio6.java
@@ -0,0 +1,36 @@
+package boletin1;
+
+import java.io.*;
+import java.util.*;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+    	
+        List<Integer> numeros = new ArrayList<>();
+        String linea;
+        int numero;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("Tema6\\src\\boletin1\\archivos\\ejercicio6out.txt"))) {
+            while ((linea = br.readLine()) != null) {
+                numero = Integer.parseInt(linea);
+                numeros.add(numero);
+            }
+
+            Collections.sort(numeros);
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter("Tema6\\src\\boletin1\\archivos\\ejercicio6in.txt", true))) {
+                for (int num : numeros) {
+                    bw.write(String.valueOf(num));
+                    bw.newLine();
+                }
+            }
+            
+            System.out.println("Terminado");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
